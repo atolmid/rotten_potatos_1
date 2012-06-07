89e476e1bb2a51c326438c78295d8a830725e286
saasbook <saasbook@saasbook.(none)>
2012-06-06 19:59:01 -0700

    added a helper ratings_s that checks the ratings hash, and when nil returns session[:ratings]

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -7,4 +7,9 @@ module MoviesHelper
   def checked?(rating) 
     @sel_ratings.include? rating 
   end
+  
+  def ratings_s(ratings)
+    (ratings == nil) ? ratings = session[:ratings].to_hash.keys : (ratings=ratings)
+  end
+  
 end
