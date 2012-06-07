331730f1c567b34cd31b7319aa7f813a27c78889
saasbook <saasbook@saasbook.(none)>
2012-06-06 18:14:15 -0700

    put session[:sort_by] = params[:sort_by] inside the params[:sort_by] condition evaluation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,20 +10,20 @@ class MoviesController < ApplicationController
     rating = Movie.ratings,
     (params[:ratings] == nil) ? ((session[:ratings]==nil) ? rating = Movie.ratings : rating = session[:ratings].to_hash.keys) : (rating = params[:ratings].to_hash.keys ; session[:ratings] = params[:ratings]),
     selected = rating,
-    (params[:sort_by] == nil) ? @order = session[:ratings] : @order = params[:sort_by],
+    (params[:sort_by] == nil) ? @order = session[:ratings] : (@order = params[:sort_by] ; session[:sort_by] = params[:sort_by],
     
     order = case @order
     when "title"
         [@col = 'title',
         @sel_ratings = selected,
         @all_ratings = Movie.ratings,
-        session[:sort_by=>"title"],
+        #session[:sort_by=>"title"],
         @movies = Movie.find_all_by_rating(rating, :order =>'title')]
     when "date"
         [@col = 'date',
         @all_ratings = Movie.ratings,
         @sel_ratings = selected,
-        session[:sort_by=>"date"],
+        #session[:sort_by=>"date"],
         @movies = Movie.find_all_by_rating(rating, :order=>'release_date')]
     else
         [@sel_ratings = selected,
