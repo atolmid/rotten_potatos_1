5240667cf3906485877bc0218030d929b59750ff
atolmid <eidimon@gmail.com>
2012-06-09 17:59:41 -0700

    changed session ratings hash - value is now true and not the same as the key

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -12,11 +12,4 @@ module MoviesHelper
     (ratings == nil) ? ((session[:ratings]==nil) ? rating = Movie.ratings : ratings = session[:ratings].to_hash.keys) : (ratings=ratings)
   end
   
-  def path(sort,ratings)
-    (sort==nil) ? sort = session[:sort_by] : sort = sort
-    (session[:ratings]==nil) ? session[:ratings] = Movie.ratings : session[:ratings] = session[:ratings]
-    (ratings == nil) ? ratings = session[:ratings] : ratings = ratings
-    redirect_to movie_path({:sort_by=>sort, :ratings=>ratings})
-  end
-  
 end
